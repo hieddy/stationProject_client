6758a4d232c2457d147555a7b5bc437e1ce77bd5
eddy <hieddy308@gmail.com>
2023-06-06 23:36:09 +0900

    [feat]: 페이지navigation

--- src/pages/Landing/LandingPage.js
+++ src/pages/Landing/LandingPage.js
@@ -1,7 +1,21 @@
+import Button from "@mui/material/Button";
+import { useNavigate } from "react-router-dom";
+import { Path } from "../../Router";
+
 const LandingPage = () => {
+  const navigate = useNavigate();
+
+  const clickHandler = () => {
+    navigate(Path.STATION);
+  };
+
   return (
     <>
-      <h1>hi</h1>
+      <h1>Hi</h1>
+      <h2>Click Here ⬇</h2>
+      <Button variant="contained" onClick={clickHandler}>
+        시작하기
+      </Button>
     </>
   );
 };

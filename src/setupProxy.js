4fade39accc77e41a8a1764f709f2eedc0d29620
eddy <hieddy308@gmail.com>
2023-06-08 02:27:04 +0900

    [test]: cors testing

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,8 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    createProxyMiddleware("/api", {
+    "/api",
+    createProxyMiddleware({
       target: "http://localhost:3001",
       changeOrigin: true,
     })
